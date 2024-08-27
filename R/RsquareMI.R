3a34b9b24bedba787182fd4ddb73a24932c10daf
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2024-08-27 13:12:26 +0200

    changed main function and tests to be appropriate for package

--- R/RsquareMI.R
+++ R/RsquareMI.R
@@ -1,48 +1,122 @@
-print.Output <- function(x) {
-  cat('R-squared SP:', '\n', x$R_squared,'\n','\n')
+print.RsquaredMI <- function(x) {
+  cat("R-squared SP:", "\n", x$R_squared, "\n", "\n")
   if (!is.null(x$beta)) {
-    cat('Beta Coefficients SP:', '\n')
+    cat("Beta Coefficients SP:", "\n")
     print(x$beta)
-    cat('\n')
+    cat("\n")
   }
   if (!is.null(x$zero)) {
-    cat('Zero Order SP:', '\n')
+    cat("Zero Order SP:", "\n")
     print(x$zero)
-    cat('\n')
+    cat("\n")
   }
 }
-RsquareSP = function(dataset,
-                     model,
-                     beta = FALSE,
-                     cor = FALSE){
-  library(lm.beta)
-  results <- list(R_squared=NULL,Beta=NULL,Zero=NULL)
-  class(results) <- "Output"
-  NumberOfImp = dataset$m
-  dataset <- complete(dataset, "long", inc = TRUE)
-  modeltemp <- lm(model,dataset)
+
+#' Calculate R-squared with Standardized Predictors
+#'
+#' This function calculates the R-squared value for a linear model applied to a multiply imputed dataset.
+#' Optionally, it can also return the standardized regression coefficients and zero-order correlations.
+#'
+#' @param dataset A multiply imputed dataset of class `mids` from the `mice` package.
+#' @param model A linear model formula specifying the model to be fitted.
+#' @param beta Logical. If `TRUE`, the function returns the standardized regression coefficients. Default is `FALSE`.
+#' @param cor Logical. If `TRUE`, the function returns the zero-order correlations between the outcome and each predictor. Default is `FALSE`.
+#'
+#' @return A list of class `RsquaredMI` containing the following elements:
+#' \item{R_squared}{The R-squared value calculated using standardized predictors.}
+#' \item{beta}{The standardized regression coefficients (if `beta = TRUE`).}
+#' \item{zero}{The zero-order correlations between the outcome and each predictor (if `cor = TRUE`).}
+#'
+#' @details The function first completes the imputed datasets using `mice::complete`.
+#' It then calculates the linear model on each imputed dataset and averages the standardized coefficients and correlations across imputations.
+#' The final R-squared value is computed as the sum of the products of the averaged standardized coefficients and averaged correlations.
+#'
+#' @import mice
+#' @importFrom lm.beta lm.beta
+#'
+#' @examples
+#' \dontrun{
+#' # Assuming `imp` is a multiply imputed dataset from `mice`
+#' model <- as.formula(y ~ x1 + x2)
+#' result <- RsquareSP(dataset = imp, model = model, beta = TRUE, cor = TRUE)
+#' print(result$R_squared)
+#' print(result$beta)
+#' print(result$zero)
+#' }
+#'
+#' @export
+RsquareSP <- function(dataset,
+                      model,
+                      beta = FALSE,
+                      cor = FALSE) {
+  results <- list(R_squared = NULL, Beta = NULL, Zero = NULL)
+  class(results) <- "RsquaredMI"
+
+  NumberOfImp <- dataset$m
+  dataset <- mice::complete(dataset, "long", inc = TRUE)
+  modeltemp <- lm(model, dataset)
   vars <- names(modeltemp$model)
   outcome <- vars[1]
   predictors <- vars[2:length(vars)]
-  meanbeta <- meancor <- rep(0, times=length(predictors))
-  for (m in 1:NumberOfImp){    
-    datasetm <- dataset[dataset[,1]==m,vars]
-    modelb <- lm(model,datasetm)
-    modelbeta <- lm.beta(modelb)
-    meanbeta <- meanbeta + modelbeta$standardized.coefficients[2:length(modelbeta$standardized.coefficients)]
-    meancor <- meancor + cor(datasetm)[1,2:ncol(datasetm)]
-  }
-  meanbeta <- meanbeta/NumberOfImp
-  meancor <- meancor/NumberOfImp
-  results$R_squared <- sum(meanbeta*meancor)
 
-  if (beta == TRUE) {
-     results$beta <- meanbeta
-     names(results$beta) <- predictors
+  meanbeta <- meancor <- rep(0, times = length(predictors))
+  for (m in 1:NumberOfImp) {
+    datasetm <- dataset[dataset[, 1] == m, vars]
+    modelb <- lm(model, datasetm)
+    modelbeta <- lm.beta::lm.beta(modelb)
+    meanbeta <- meanbeta + modelbeta$standardized.coefficients[2:length(modelbeta$standardized.coefficients)]
+    meancor <- meancor + cor(datasetm)[1, 2:ncol(datasetm)]
   }
-  if (cor == TRUE) {
+  meanbeta <- meanbeta / NumberOfImp
+  meancor <- meancor / NumberOfImp
+  results$R_squared <- sum(meanbeta * meancor)
+
+  if (beta) {
+    results$beta <- meanbeta
+    names(results$beta) <- predictors
+  }
+  if (cor) {
     results$zero <- meancor
     names(results$zero) <- predictors
-  } 
+  }
+
+  return(results)
+}
+
+RsquareSP <- function(dataset,
+                      model,
+                      beta = FALSE,
+                      cor = FALSE) {
+  results <- list(R_squared = NULL, Beta = NULL, Zero = NULL)
+  class(results) <- "RsquaredMI"
+
+  NumberOfImp <- dataset$m
+  dataset <- mice::complete(dataset, "long", inc = TRUE)
+  modeltemp <- lm(model, dataset)
+  vars <- names(modeltemp$model)
+  outcome <- vars[1]
+  predictors <- vars[2:length(vars)]
+
+  meanbeta <- meancor <- rep(0, times = length(predictors))
+  for (m in 1:NumberOfImp) {
+    datasetm <- dataset[dataset[, 1] == m, vars]
+    modelb <- lm(model, datasetm)
+    modelbeta <- lm.beta::lm.beta(modelb)
+    meanbeta <- meanbeta + modelbeta$standardized.coefficients[2:length(modelbeta$standardized.coefficients)]
+    meancor <- meancor + cor(datasetm)[1, 2:ncol(datasetm)]
+  }
+  meanbeta <- meanbeta / NumberOfImp
+  meancor <- meancor / NumberOfImp
+  results$R_squared <- sum(meanbeta * meancor)
+
+  if (beta) {
+    results$beta <- meanbeta
+    names(results$beta) <- predictors
+  }
+  if (cor) {
+    results$zero <- meancor
+    names(results$zero) <- predictors
+  }
+
   return(results)
 }
--- tests/test.R
+++ tests/test.R
@@ -1,82 +1,80 @@
-setwd("P:/VanGinkelKarch3/")
-library(mice)
 library(MASS)
-source("RsquareMI.r")
-#Data properties
-mu = c(2,5,10,20)
-sigma = matrix(c(5,0,0,3,0,5,1,0,0,1,5,0,3,0,0,10),nrow = 4, ncol = 4)
-sigmaerror = 0.6
-muerror = 0
-NumberOfRsquareds = 2
-Populationbs = c(0.2,0.1,0.1,-0.1,0)
-#Populationbs = c(0,0,0,0,0)
-n=500
-percent=0.25
-NumberOfImp = 25
+library(mice)
+# Data properties
+mu <- c(2, 5, 10, 20)
+sigma <- matrix(c(5, 0, 0, 3, 0, 5, 1, 0, 0, 1, 5, 0, 3, 0, 0, 10), nrow = 4, ncol = 4)
+sigmaerror <- 0.6
+muerror <- 0
+NumberOfRsquareds <- 2
+Populationbs <- c(0.2, 0.1, 0.1, -0.1, 0)
+# Populationbs = c(0,0,0,0,0)
+n <- 500
+percent <- 0.25
+NumberOfImp <- 25
 
-#Simulating data
-sigmaYhat = 0
-ii = 1
-for (i in 1:nrow(sigma)){
-  ii = ii + 1
-  jj = 1 
-  for (j in 1:ncol(sigma)){
-    jj = jj + 1
-    sigmaYhat = sigmaYhat + Populationbs[ii]*Populationbs[jj]*sigma[i,j]
+# Simulating data
+sigmaYhat <- 0
+ii <- 1
+for (i in 1:nrow(sigma)) {
+  ii <- ii + 1
+  jj <- 1
+  for (j in 1:ncol(sigma)) {
+    jj <- jj + 1
+    sigmaYhat <- sigmaYhat + Populationbs[ii] * Populationbs[jj] * sigma[i, j]
   }
 }
-  
-sigmaY <- sigmaYhat + sigmaerror
-#Seed = 234 
-#set.seed(Seed)
-X = mvrnorm(n = n, mu=mu, Sigma=sigma)        
-error = mvrnorm(n=n,mu=0, Sigma=sigmaerror)
-colnames(X) = c("X1","X2","X3","X4")
-intercept = rep(1,times=n)
-interceptX = cbind(intercept, X)
-Y = rep(0,times=n)
-for (j in 1:length(Populationbs)) {Y = Y + Populationbs[j]*interceptX[,j]}
-Y = Y + error
-colnames(Y) = "Y"
-XY = cbind(X,Y)     
-   
 
-#Simulating missing data
-Numberofmissing = percent*(nrow(XY)*ncol(XY))
-#Seed = 235 
-#set.seed(Seed) 
-randommatrix = runif((nrow(XY)*(ncol(XY)-1))) 
-randommatrix = matrix(randommatrix,nrow(XY),(ncol(XY)-1))
-missingmatrix = matrix(1,nrow(XY),(ncol(XY)-1)) 
-weightmatrix <- matrix(1, nrow(XY), (ncol(XY)-1)) 
-randommatrix <- randommatrix*weightmatrix
-stoploop = FALSE
-for (k in 1:Numberofmissing){
-  for (ii in 1:nrow(randommatrix)){
-    for (jj in 1:ncol(randommatrix)){
-      if (randommatrix[ii,jj] == max(randommatrix) & any(randommatrix[ii,-jj] != 0)) {
-        missingmatrix[ii,jj] = 0
-        randommatrix[ii,jj] = 0 
-        stoploop = TRUE
-        break             
+sigmaY <- sigmaYhat + sigmaerror
+# Seed = 234
+# set.seed(Seed)
+X <- mvrnorm(n = n, mu = mu, Sigma = sigma)
+error <- mvrnorm(n = n, mu = 0, Sigma = sigmaerror)
+colnames(X) <- c("X1", "X2", "X3", "X4")
+intercept <- rep(1, times = n)
+interceptX <- cbind(intercept, X)
+Y <- rep(0, times = n)
+for (j in 1:length(Populationbs)) {
+  Y <- Y + Populationbs[j] * interceptX[, j]
+}
+Y <- Y + error
+colnames(Y) <- "Y"
+XY <- cbind(X, Y)
+
+
+# Simulating missing data
+Numberofmissing <- percent * (nrow(XY) * ncol(XY))
+# Seed = 235
+# set.seed(Seed)
+randommatrix <- runif((nrow(XY) * (ncol(XY) - 1)))
+randommatrix <- matrix(randommatrix, nrow(XY), (ncol(XY) - 1))
+missingmatrix <- matrix(1, nrow(XY), (ncol(XY) - 1))
+weightmatrix <- matrix(1, nrow(XY), (ncol(XY) - 1))
+randommatrix <- randommatrix * weightmatrix
+stoploop <- FALSE
+for (k in 1:Numberofmissing) {
+  for (ii in 1:nrow(randommatrix)) {
+    for (jj in 1:ncol(randommatrix)) {
+      if (randommatrix[ii, jj] == max(randommatrix) & any(randommatrix[ii, -jj] != 0)) {
+        missingmatrix[ii, jj] <- 0
+        randommatrix[ii, jj] <- 0
+        stoploop <- TRUE
+        break
       }
     }
     if (stoploop == TRUE) {
-      stoploop = FALSE
+      stoploop <- FALSE
       break
     }
   }
 }
 XYmis <- XY
-XYmis[,2:ncol(XYmis)][missingmatrix==0] = NA
+XYmis[, 2:ncol(XYmis)][missingmatrix == 0] <- NA
 seed <- 554
- 
-XYimp  <- mice(XYmis, maxit = 10, m = NumberOfImp, printFlag = FALSE, seed = seed)
 
-model = as.formula("Y~X1+X2+X3+X4")
+XYimp <- mice(XYmis, maxit = 10, m = NumberOfImp, printFlag = FALSE, seed = seed)
 
-results = RsquareSP(XYimp, model, cor=TRUE, beta=TRUE) 
+model <- as.formula("Y~X1+X2+X3+X4")
+
+results <- RsquareSP(XYimp, model, cor = TRUE, beta = TRUE)
 
 results
-
-        
